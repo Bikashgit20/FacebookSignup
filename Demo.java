968e59eff622a43a4a1f09bd2d0c92ba06f75b76
bikash <bikashkar0127@gmail.com>
2022-12-02 19:57:07 +0530

    demo file for learning

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package step;
+
+public class Demo 
+{
+	public void add()
+	{
+		System.out.println("not related to the code just for demo purpose");
+	}
+	public void minus()
+	{
+		System.out.println("check if showing on loginPage branch");
+	}
+
+}
